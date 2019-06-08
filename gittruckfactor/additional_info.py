4c504ba084be0584583f4d327b08ceb18e49a2b7
samoliveira <samuelmoliveira0@gmail.com>
2019-06-08 00:40:22 -0300

    arquivo com dados adicionais atraves do script additional_info.py

--- gittruckfactor/additional_info.py
+++ gittruckfactor/additional_info.py
@@ -12,10 +12,10 @@ def main(ANALYZED_REPO, COMMIT_FILE_INFO_LOG):
     all_commits = list(repo.iter_commits())
     repo.git.checkout(all_commits[0])
 
-    with open(COMMIT_FILE_INFO_LOG) as file_info:
+    with open(COMMIT_FILE_INFO_LOG,'r') as file_info:
         actual_commit = ""
         for line in file_info:
-            commit_sha, status, old ,filepath = line.split(";")
+            commit_sha, status, old ,filepath = line.split("\n")[0].split(";")
             if commit_sha != actual_commit:
                 actual_commit = commit_sha
                 repo.git.checkout(actual_commit)
@@ -24,16 +24,21 @@ def main(ANALYZED_REPO, COMMIT_FILE_INFO_LOG):
                 loc = i.__dict__['nloc']
 
                 cyc = 0
-                for f in i.__dict__['function_list']:
-                    cyc = cyc + f['cyclomatic_complexity']
-                mod = Commit(actual_commit).stats.lines
+                for f in i.function_list:
+                    #print(f.__dict__['cyclomatic_complexity'])
+                    cyc = cyc + f.__dict__['cyclomatic_complexity']
+                mod = repo.commit(actual_commit).stats.total['lines']
+                with open ("additional_info.log", "a") as f_i:
+                    print(line.split("\n")[0]+";"+str(loc)+";"+str(mod)+";"+str(cyc))
+                    print(line.split("\n")[0]+";"+str(loc)+";"+str(mod)+";"+str(cyc), file=f_i)
 
-                print(loc,mod,cyc)
-
+    repo.git.checkout(all_commits[0])
 
 if __name__ == '__main__':
+
+    print(len(sys.argv), "len")
     if len(sys.argv) == 3:
-        print(sys.argv[1], sys.argv[2])
         main(sys.argv[1], sys.argv[2])
+
     else:
-        print("Error number of args are wrong expected 2")
+        print("Error number of args are wrong expected 3")
